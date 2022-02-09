e9a4d79e5d9e16b3f631a7eeac2438c51dcdd042
gashig007 <rustamgashigulin2001@gmail.com>
2022-02-09 16:46:06 +0600

    set commit glide

--- app/src/main/java/com/example/homework31/MainActivity.java
+++ app/src/main/java/com/example/homework31/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.homework31;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView_car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView_car =  findViewById(R.id.image_car);
+        Glide.with(MainActivity.this).load("https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg").into(imageView_car);
     }
 }
